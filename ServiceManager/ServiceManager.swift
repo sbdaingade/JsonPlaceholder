cdbcd7282e470e2ebb3ed9120c1ad30f1ae25f70
Sachin Daingade <sbdaingade@gmail.com>
2019-11-07 23:05:52 +0530

    first commit

---
+++ ServiceManager/ServiceManager.swift
@@ -0,0 +1,9 @@
+//
+//  ServiceManager.swift
+//  JSONPlaceholder
+//
+//  Created by Sachin Daingade on 06/11/19.
+//  Copyright © 2019 Sachin Daingade. All rights reserved.
+//
+
+import Foundation
---
+++ View/AlbumCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumCell.swift
+//  JSONPlaceholder
+//
+//  Created by Sachin Daingade on 07/11/19.
+//  Copyright © 2019 Sachin Daingade. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
